314097b7ce95e06b9b01f22927a88ac731681bef
edw777 <wanged1110@gmail.com>
2023-09-12 18:33:23 -0400

    implementing problem 1 and 5

--- assigns/assign0/MySolution/assign0-1.ml
+++ assigns/assign0/MySolution/assign0-1.ml
@@ -1,69 +1,20 @@
 (* ****** ****** *)
-(*
-Assign0: Warmup!
-*)
-(* ****** ****** *)
 
-(*
-fun fact(x: int): int =
-  if x > 0 then x * fact(x-1) else 1
-*)
-
-(*
-Assign0-1: 10 points
-Please find the first integer N such that the
-evaluation of fact(N) in OCaml yields an Overflow
-exception.
-*)
-
-let rec factorial(n) = 
-  if n > 0 then n * factorial(n-1) else 1
-
-let rec loop(n) = 
-  if factorial(n) > 0 then loop(n+1) else n
-
-let myAns = loop(0);;
+let rec
+fact(x: int): int =
+if x > 0 then fact(x-1) * x  else 1
+;;
 
 (* ****** ****** *)
 
-(*
-Assign0-2: 10 points
-Please implement a function that tests whether a
-given natural number is a prime:
-fun isPrime(n0: int): bool
-*)
-   
-
+let rec
+myloop(x: int): int =
+if fact(x) = 0 then x else myloop(x+1)
 
 (* ****** ****** *)
-
-(*
-Assign0-3: 10 points
-Please implement a function that converts a given
-integer to a string that represents the integer:
-fun int2str(i0: int): string
-*)
-
-
+let myans = myloop(0)
+(* ****** ****** *)
+;;
 (* ****** ****** *)
 
-(*
-Assign0-4: 10 points
-Please implement a function that converts a given
-string to an integer:
-fun str2int(cs: string): int
-In particular, it is expected that str2int(int2str(x)) = x
-*)
-
-(* ****** ****** *)
-
-(*
-Assign0-5: 10 points
-Please implement a function that returns the reverse of
-a given string:
-fun stringrev(cs: string): string
-*)
-
-(* ****** ****** *)
-
-(* end of [CS320-2023-Fall-assign0.ml] *)
+(* end of [CS320-2023-Fall-assign0-1.ml] *)
--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -1,12 +1,36 @@
+(* ****** ****** *)
+(* ****** ****** *)
+
+let chr = Char.chr
+let ord = Char.code
+let str(c0) = String.make 1 c0
+;;
+(* ****** ****** *)
+
+let string_init = String.init
+let string_length = String.length
+let string_get(cs, i0) = String.get cs i0
+;;
+(* ****** ****** *)
 (*
 Assign0-5: 10 points
 Please implement a function that returns the reverse of
 a given string:
 fun stringrev(cs: string): string
 *)
-let stringrev(cs: string): string =
-  let len = String.length cs in
+let concat_helper(index: int) (s1: string) (s2: string): char =
+  if index <= string_length(s1) - 1 then string_get(s1, index) else string_get(s2, index - string_length(s1))
 
-let rec reverse
+let string_concatenate (s1: string) (s2: string): string =
+  string_init(string_length(s1) + string_length(s2)) (fun i -> concat_helper(i)(s1)(s2))
+
+let rec stringhelper(cs:string) (index:int): string =
+  if string_length(cs) - index -1 = 0 then str(string_get(cs, 0)) else
+    string_concatenate(str(string_get(cs, (string_length(cs)-1-index)))) (stringhelper(cs) (index + 1))
+
+let stringrev(cs: string): string =
+  if string_length(cs) > 0 then stringhelper(cs) (0) else cs
+
+let test = stringrev("Mensun");;
 
 (* ****** ****** *)
