559fcb7ac6e523ddd6c078801e4b123b499b9090
edw777 <wanged1110@gmail.com>
2023-09-19 19:52:50 -0400

    assign1-6 commit

--- assigns/assign1/MySolution/assign1-5.ml
+++ assigns/assign1/MySolution/assign1-5.ml
@@ -1,3 +1,4 @@
+#use "./../../../classlib/OCaml/MyOCaml.ml";;
 (*
 assign1-5: 20 points
 
@@ -20,3 +21,13 @@ string_longest_ascend returns "123456"
 For instance, given "1234511111", the function
 string_longest_ascend returns "111111".
 *)  
+let rec substring (xs: string) (trav_index) (start: int) (finish: int): string = 
+  if trav_index > finish then "" else string_get_at(xs) (trav_index) ^ substring (xs:string) (trav_index+1) (start) (finish)
+
+let rec helper (xs: string) (index: int) (trav_index: int) (start: int) (finish: int): string = 
+  if index >= string_length(xs) - 1 then substring (xs) (index) (index) (finish) else
+    if string_get_at(xs) (index) <= string_get_at(xs) (index+1) then helper(xs) (index) (index) (trav_index) (index) (index+1) else
+      helper(xs)
+ 
+let string_longest_ascend(xs: string): string = 
+  helper (xs) (0) (1) (0) (0)
---
+++ assigns/assign1/MySolution/assign1-6.ml
@@ -0,0 +1,53 @@
+#use "./../../../classlib/OCaml/MyOCaml.ml";;
+(* assign1-6: 20 bonus points
+A 4-letter sequence abcd is 1324-like
+if a < c < b < d holds. For instance, 1234 is
+not 132-like; but 2547 is 1324-like.
+
+A string is 1324-avoid if there is no subsequence
+abc in this string that is 1324-like. Note that a
+subsequence of a string may contain NON-CONSEQUTIVE
+letters of the string.
+
+For instance, 123456789 is 1324-avoid;
+For instance, 987654321 is 1324-avoid;
+For instance, 123465789 is not 132-avoid since the
+subsequence 1657 is 1324-like.
+
+Please implement a function string_avoid_1324 that
+checks if a given string is 1324-avoid; the function
+returns true if and only if the given string is 1324-
+avoid.
+
+fun string_avoid_1324(cs: string): bool
+*)
+let rec helper_4(cs:string) (curr:char) (curr_index:int) (index: int) (b_holder: int): bool =
+  if index >= string_length(cs) - 1 then true else
+    if index > string_length(cs) then helper_4(cs) (string_get_at(cs) (curr_index)) (curr_index+1) (curr_index+1) (b_holder) else
+      if string_get_at(cs) (curr_index) < string_get_at(cs)(curr_index +1) && string_get_at(cs) (curr_index+1) > string_get_at(cs) (b_holder) then false else 
+        helper_4(cs) (string_get_at(cs) (curr_index+1)) (curr_index+1) (curr_index+2) (b_holder)
+
+let rec helper_2(cs:string) (curr:char) (curr_index:int) (index: int) (a_holder: int): bool = 
+  if curr_index >= string_length(cs) - 2 && index == string_length(cs)-1 then true else
+    if index > string_length(cs) then helper_2(cs) (string_get_at(cs) (curr_index)) (curr_index+1) (curr_index+1) (a_holder) else
+      if string_get_at(cs) (curr_index) > string_get_at(cs)(curr_index +1) then helper_4(cs) (string_get_at(cs) (curr_index+1)) (curr_index) (index) (curr_index) else 
+        helper_2(cs) (string_get_at(cs) (curr_index+1)) (curr_index+1) (curr_index+2) (a_holder)
+
+let rec helper_3(cs:string) (curr: char) (curr_index: int) (index: int) (a_holder: int) : bool = 
+  if curr_index >= string_length(cs) - 3 && index == string_length(cs)-1 then true else
+  if index > string_length(cs) then helper_3(cs) (string_get_at(cs) (curr_index+1)) (curr_index+1) (curr_index+1) (a_holder) else
+    if string_get_at(cs) (curr_index) > string_get_at(cs)(curr_index+1) then helper_2(cs) (string_get_at(cs) (curr_index+1)) (curr_index) (index) (a_holder)
+    else helper_3(cs) (string_get_at(cs) (curr_index+1)) (curr_index+1) (curr_index+2) (a_holder)
+
+let rec helper_1(cs:string) (curr: char) (curr_index: int) (index: int): bool = 
+  if curr_index >= string_length(cs) - 4 && index == string_length(cs)-1 then true else
+  if index > string_length(cs) then helper_1(cs) (string_get_at(cs) (curr_index+1)) (curr_index+1) (curr_index+1) else
+    if string_get_at(cs) (curr_index) < string_get_at(cs) (curr_index + 1) then helper_3(cs) (string_get_at(cs) (curr_index+1)) (curr_index) (index) (curr_index)
+    else helper_1(cs) (string_get_at(cs) (curr_index+1)) (curr_index+1) (curr_index+2)
+
+let string_avoid_1324 (cs: string): bool = 
+  if string_length(cs) == 0 then true else helper_1 (cs) (string_get_at(cs) (0)) (0) (1)
+
+let test = string_avoid_1324("1657")
+
+(* ****** ****** *)
