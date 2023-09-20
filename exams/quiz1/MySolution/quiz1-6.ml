7d0bf72ed56a3b6d58d3cceaf96f875d8785f117
edw777 <wanged1110@gmail.com>
2023-09-19 22:23:54 -0400

    updating quiz 1-6

--- exams/quiz1/MySolution/quiz1-6.ml
+++ exams/quiz1/MySolution/quiz1-6.ml
@@ -4,13 +4,13 @@
 
 let pp x y = fun f -> f(x,y)
 
-let ff = fun x y -> x - y
+let ff (x, y) = y - x
 
 (* Give an implementation of ff that
    makes the following assertions pass *)
 
-(*let () = assert (pp 200 100 ff = 100)
-let () = assert (pp 100 200 ff = -100) *)
+let () = assert (pp 200 100 ff = 100)
+let () = assert (pp 100 200 ff = -100)
 
 
 
